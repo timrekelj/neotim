53846d75165e3030c844fcc1d2cbedd9d8a64f1b
Tim Rekelj <rekeljtim@gmail.com>
2022-08-03 11:33:26 +0200

    add: some packer configuration

--- lua/packer.lua
+++ lua/packer.lua
@@ -5,6 +5,22 @@
 -- Plugin manager: packer.nvim
 -- url: https://github.com/wbthomason/packer.nvim
 
+-- Automatically install packer
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth',
+    '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+  vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
+end
+
 -- Autocommand that reloads neovim whenever you save the packer_init.lua file
 vim.cmd [[
   augroup packer_user_config
@@ -20,7 +36,7 @@ if not status_ok then
 end
 
 -- Install plugins
-return require('packer').startup(function()
+return packer.startup(function(use)
   -- Package manager
   use 'wbthomason/packer.nvim'
 

88d7b0c48b840e7299878f6b308b29958336bd71
Tim Rekelj <rekeljtim@gmail.com>
2022-08-04 23:04:09 +0200

    fix: changed file_browser to actually show hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,7 +13,6 @@ end
 telescope.setup {
     pickers = {
         find_files = {
-            hidden = true,
             theme = "dropdown",
             previewer = false,
         }
@@ -28,6 +27,6 @@ telescope.setup {
 require("telescope").load_extension "file_browser"
 
 vim.api.nvim_set_keymap('n', '<leader>p', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>n', ":Telescope file_browser<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ":Telescope file_browser hidden=true<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>b', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>g', "<cmd>lua require('telescope.builtin').git_branches()<CR>", { noremap = true })

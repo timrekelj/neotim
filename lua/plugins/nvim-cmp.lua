b0b4f5b8f1902832233cb3994e711a5cceca0592
Tim Rekelj <tim.rekelj@proton.me>
2022-11-08 17:12:06 +0100

    fixed cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,79 +11,118 @@ end
 require("luasnip/loaders/from_vscode").lazy_load()
 
 local check_backspace = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+    local col = vim.fn.col(".") - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
---   פּ ﯟ   some other good icons
 local kind_icons = {
     Text = "",
-    Method = "m",
-    Function = "",
-    Constructor = "",
-    Field = "",
-    Variable = "",
-    Class = "",
-    Interface = "",
-    Module = "",
-    Property = "",
-    Unit = "",
-    Value = "",
-    Enum = "",
-    Keyword = "",
-    Snippet = "",
-    Color = "",
-    File = "",
-    Reference = "",
-    Folder = "",
-    EnumMember = "",
-    Constant = "",
-    Struct = "",
-    Event = "",
-    Operator = "",
-    TypeParameter = "",
+   	Method = "",
+   	Function = "",
+   	Constructor = "",
+   	Field = "",
+   	Variable = "",
+   	Class = "",
+   	Interface = "",
+   	Module = "",
+   	Property = "",
+   	Unit = "",
+   	Value = "",
+   	Enum = "",
+   	Keyword = "",
+   	Snippet = "",
+   	Color = "",
+   	File = "",
+   	Reference = "",
+   	Folder = "",
+   	EnumMember = "",
+   	Constant = "",
+   	Struct = "",
+   	Event = "",
+   	Operator = "",
+   	TypeParameter = "",
 }
--- find more here: https://www.nerdfonts.com/cheat-sheet
 
-cmp.setup {
+cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
-    mapping = {
-        ['<C-m>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-        -- If you don't set up snippets in the section below, this might crash, either go through the "Snippets" section or remove any `luasnip` related code from this config.  
-        ['<Tab>'] = function(fallback)
+
+    mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+      	["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        -- Accept currently selected item. If none selected, `select` first item.
+        -- Set `select` to `false` to only confirm explicitly selected items.
+      	["<CR>"] = cmp.mapping.confirm({ select = true }),
+      	["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+      	    elseif luasnip.expandable() then
+                luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+      	  	    luasnip.expand_or_jump()
+      	  	elseif check_backspace() then
+      	  	    fallback()
+      	  	else
+      	  	    fallback()
+      	  	end
+        end, {
+            "i",
+            "s",
+        }),
+      	["<S-Tab>"] = cmp.mapping(function(fallback)
+      	  	if cmp.visible() then
+      	  	    cmp.select_prev_item()
+      	  	elseif luasnip.jumpable(-1) then
                 luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end,
+      	  	else
+      	  	    fallback()
+      	  	end
+      	end, {
+      	  	"i",
+      	  	"s",
+      	}),
+    }),
+    formatting = {
+      	fields = { "kind", "abbr", "menu" },
+      	format = function(entry, vim_item)
+            vim_item.kind = kind_icons[vim_item.kind]
+            vim_item.menu = ({
+                nvim_lsp = "",
+                nvim_lua = "",
+                luasnip = "",
+                buffer = "",
+                path =  "",
+                emoji = "",
+            })[entry.source.name]
+      	    return vim_item
+      	end,
     },
     sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+        { name = "nvim_lsp" },
+      	{ name = "nvim_lua" },
+      	{ name = "luasnip" },
+      	{ name = "buffer" },
+      	{ name = "path" },
     },
-}
+    confirm_opts = {
+      	behavior = cmp.ConfirmBehavior.Replace,
+      	select = false,
+    },
+    window = {
+      	completion = cmp.config.window.bordered(),
+      	documentation = cmp.config.window.bordered(),
+    },
+    experimental = {
+      	ghost_text = true,
+    },
+})

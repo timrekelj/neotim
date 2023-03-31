8aa96f1d1375711787ac23bc6cb1bdea62c7121f
Tim Rekelj <tim.rekelj@proton.me>
2023-03-31 11:55:00 +0200

    fixed broken config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,90 +1,11 @@
------------------------------------------------------------
--- Plugin manager configuration file
------------------------------------------------------------
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
 
--- Plugin manager: lazyvim
--- url: https://github.com/folke/lazy.nvim
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Install plugins
-local plugins = {
-    -- LSP
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
-        }
-    }
-
-    -- Icons
-    use 'kyazdani42/nvim-web-devicons'
-
-    -- Autopair
-    use {
-        config = function()
-            require('nvim-autopairs').setup {}
-        end
-    }
-
-    -- Themes
-    use 'Mofiqul/dracula.nvim'
-    use 'folke/tokyonight.nvim'
-
-    -- Syntax highlihting
-    use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-
-    -- Easier comments
-    use 'tpope/vim-commentary'
-
-    -- Searching
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-
-    use({
-        "kylechui/nvim-surround",
-        tag = "*"
-    })
-
-    use 'theprimeagen/harpoon'
-
-    use 'mbbill/undotree'
-
-    use 'tpope/vim-fugitive'
-
-    -- Org mode
-    use {'nvim-orgmode/orgmode', config = function()
-        require('orgmode').setup{}
-    end}
-} 
-
-require("lazy").setup(plugins, opts)
+lsp.setup()

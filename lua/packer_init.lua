b0982842777375a007e4b6cefa1633a2baf5927a
Tim Rekelj <tim.rekelj@proton.me>
2023-09-25 00:33:38 +0200

    added vim-closer

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -61,5 +61,6 @@ return packer.startup(function(use)
         'nvim-telescope/telescope.nvim', tag = '0.1.1', -- file searching
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use 'rstacruz/vim-closer'
     use 'theprimeagen/harpoon'
 end)

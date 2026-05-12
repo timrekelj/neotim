17cb51f6f76f065f024a8b7572d4cb8f9bc075b5
Tim Rekelj <hello@timrekelj.si>
2026-05-12 10:39:56 +0200

    white cursor while moving

--- lua/plugins/smear_cursor.lua
+++ lua/plugins/smear_cursor.lua
@@ -3,6 +3,8 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    require("smear_cursor").setup()
+    require("smear_cursor").setup({
+      cursor_color = "#FFFFFF",
+    })
   end,
 }
